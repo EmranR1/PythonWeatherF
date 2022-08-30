6f46142079a80dada4e9ae1158d1bdc051c522ca
EmranR1 <80667712+EmranR1@users.noreply.github.com>
2022-08-30 16:22:40 +1000

    header user agent is a string that allows the server to identify the O.S and application

--- weatherpython.py
+++ weatherpython.py
@@ -1,5 +1,8 @@
 #weather report
 #importing BeautifulSoup library
+from msilib import Win64
+from webbrowser import Mozilla
+from wsgiref import headers
 from bs4 import BeautifulSoup
 import requests
-
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)     AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
