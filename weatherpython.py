0fdf024282a90eca3d81e40065244243898660ed
EmranR1 <80667712+EmranR1@users.noreply.github.com>
2022-08-30 16:54:06 +1000

    defining the weather function

--- weatherpython.py
+++ weatherpython.py
@@ -1,8 +1,11 @@
 #weather report
 #importing BeautifulSoup library
-from msilib import Win64
-from webbrowser import Mozilla
-from wsgiref import headers
 from bs4 import BeautifulSoup
 import requests
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)     AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+def weather(city):
+    city=city.replace(" ","+")
+    res=requests.get(f'https://www.google.com/search?q={city}&oq={city}'
+    f'&aqs=chrome.0.35i39l2j0l4j46j69i60.6128j1j7&sourceid='
+    f'chrome&ie=UTF-8',headers=headers)
+    soup = BeautifulSoup(res.text,'html.parser')
